9fd63c18fb44d1dc499e6d712401dae708501c67
Kumar <2403808@cognizant.com>
2025-06-22 21:55:29 +0530

    smoke test case add

--- src/main/java/com/pages/Formpage.java
+++ src/main/java/com/pages/Formpage.java
@@ -1,43 +1,83 @@
 package com.pages;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FormPage {
 	WebDriver driver;
 	WebDriverWait wait;
+	Select s;
+
 	public FormPage(WebDriver driver) {
 		this.driver = driver;
-		wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-		PageFactory.initElements(driver,this);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		PageFactory.initElements(driver, this);
 	}
-	@FindBy(xpath="//input[@id='name']")
+
+	@FindBy(xpath = "//input[@id='name']")
 	WebElement name;
-	
-	@FindBy(xpath="//select[@id='experience']")
+
+	@FindBy(xpath = "//select[@id='experience']")
 	WebElement yoe;
-	
-	@FindBy(xpath="//input[@id='skills']")
+
+	@FindBy(xpath = "//input[@id='skills']")
 	WebElement skills;
-	
-	@FindBy(xpath="//input[@id='websites']")
+
+	@FindBy(xpath = "//input[@id='websites']")
 	WebElement websites;
-	
-	@FindBy(xpath="//input[@id='apps']")
+
+	@FindBy(xpath = "//input[@id='apps']")
 	WebElement apps;
-	
-	@FindBy(xpath="//button[@id='measureBtn']")
+
+	@FindBy(xpath = "//button[@id='measureBtn']")
 	WebElement submit;
-	
-	@FindBy(xpath="//div[@id='result']")
+
+	@FindBy(xpath = "//div[@id='result']")
 	WebElement result;
-	
-	@FindBy(xpath="//div[@id='result']/h2")
+
+	@FindBy(xpath = "//div[@id='result']/h2")
 	WebElement resultName;
-	
+
+	@FindBy(xpath = "//div[@class='header']/h1")
+	WebElement header;
+
+	@FindBy(xpath = "//img[@alt='user']")
+	WebElement img;
+
+	public boolean isHeaderVisible() {
+		return header.isDisplayed();
+	}
+
+	public boolean cardDisplayedAfterSubmit() throws NoSuchElementException{
+		name.sendKeys("aman");
+		submit.click();
+		try {
+			WebElement result = wait
+					.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='result']")));
+			return result.isDisplayed();
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean isDropDownVisible() {
+		s = new Select(yoe);
+		s.selectByValue("1");
+		String selectedValue = s.getFirstSelectedOption().getDomAttribute("value");
+		return "1".equals(selectedValue);
+	}
+
+	public boolean isUserImageVisible() {
+		return img.isDisplayed();
+	}
 }
--- src/test/java/com/test/smoke/HomeTest.java
+++ src/test/java/com/test/smoke/HomeTest.java
@@ -1,5 +1,55 @@
 package com.test.smoke;
 
-public class HomeTest {
 
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.baseTest.BaseTest;
+import com.pages.AppsField;
+import com.pages.FormPage;
+import com.pages.NameField;
+import com.pages.SkillField;
+import com.pages.WebSiteField;
+import com.pages.YearsField;
+
+public class HomeTest extends BaseTest{
+	@Test(priority=1)
+	public void homePageVisibleTest() {
+		FormPage fp = new FormPage(driver);
+		Assert.assertTrue(fp.isHeaderVisible());
+	}
+	@Test(priority=2)
+	public void resultCardVisibleTest() {
+		FormPage fp = new FormPage(driver);
+		Assert.assertTrue(fp.cardDisplayedAfterSubmit());
+	}
+	@Test(priority=3)
+	public void allFieldsEnabledTest() {
+		AppsField af = new AppsField(driver);
+		NameField nf= new NameField(driver);
+		SkillField sf= new SkillField(driver);
+		WebSiteField wf = new WebSiteField(driver);
+		YearsField yf = new YearsField(driver);
+		Boolean flag = af.isEnabled() && nf.isEnabled() && sf.isEnabled() && wf.isEnabled() && yf.isEnabled();
+		Assert.assertTrue(flag);
+	}
+	@Test(priority=4)
+	public void neccessaryFieldsMandatoryTest() {
+		AppsField af = new AppsField(driver);
+		NameField nf= new NameField(driver);
+		WebSiteField wf = new WebSiteField(driver);
+		Boolean flag = af.isMandatory() && nf.isMandatory() && wf.isMandatory();
+		Assert.assertTrue(flag);
+	}
+	@Test(priority=5)
+	public void dropDownVisibleTest() {
+		FormPage fp = new FormPage(driver);
+		Assert.assertTrue(fp.isDropDownVisible());
+	}
+	@Test(priority=6)
+	public void userImageTest() {
+		FormPage fp = new FormPage(driver);
+		Assert.assertTrue(fp.isUserImageVisible());
+	}
 }
